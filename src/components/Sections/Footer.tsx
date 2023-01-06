9d8d2304b9d535be48f96b9c527bedf7886d5964
Dallas Johnson <dallasajohnson14@gmail.com>
2023-01-06 15:36:52 -0800

    Fix copyright format

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -19,7 +19,7 @@ const Footer: FC = memo(() => (
       <div className="flex gap-x-4 text-neutral-500">
         <Socials />
       </div>
-      <span className="text-sm text-neutral-700">© Copyright 2023 Dallas Johnson</span>
+      <span className="text-sm text-neutral-700">Copyright © 2023 Dallas Johnson</span>
     </div>
   </div>
 ));
