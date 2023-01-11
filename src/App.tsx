5c0d0743e62162699bc72094d8d9d79a6e8e4321
Dallas Johnson <dallasajohnson14@gmail.com>
2023-01-11 10:07:16 -0800

    Make metadata static and remove react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
-import { HelmetProvider } from 'react-helmet-async';
-
 import About from './components/Sections/About';
 import Contact from './components/Sections/Contact';
 import Footer from './components/Sections/Footer';
 import Header from './components/Sections/Header';
 import Hero from './components/Sections/Hero';
-import Page from './components/Layout/Page';
 import Portfolio from './components/Sections/Portfolio';
 import Resume from './components/Sections/Resume';
 import Testimonials from './components/Sections/Testimonials';
 
 function App() {
   return (
-    <HelmetProvider>
-      <Page title="Dallas Johnson's Landing"
-       description="A snapshot of Dallas's accomplishments, interests, and work life."
-      >
-        <Header />
-        <Hero />
-        <About />
-        <Resume />
-        <Portfolio />
-        <Testimonials />
-        <Contact />
-        <Footer />
-      </Page>
-    </HelmetProvider>
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Resume />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
