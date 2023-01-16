80ac1ebe0c8363675212119b140ab1cbfc594140
Dallas Johnson <dallasajohnson14@gmail.com>
2023-01-16 13:30:23 -0800

    Fix resume formatting at small sizes

--- src/components/Sections/Resume/TimelineItem.tsx
+++ src/components/Sections/Resume/TimelineItem.tsx
@@ -5,13 +5,13 @@ import {TimelineItemProps} from '../../../data/dataDef';
 const TimelineItem: FC<{item: TimelineItemProps}> = memo(({item}) => {
   const {title, date, location, content} = item;
   return (
-    <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
+    <div className="flex flex-col pb-8 text-left last:pb-0">
       <div className="flex flex-col pb-4">
-        <h2 className="text-xl font-bold">{title}</h2>
+        <h2 className="text-xl font-bold text-center md:text-left">{title}</h2>
         <div className="flex items-center justify-center gap-x-2 md:justify-start">
-          <span className="flex-1 text-sm font-medium italic sm:flex-none">{location}</span>
+          <span className="flex-none text-sm font-medium italic">{location}</span>
           <span>•</span>
-          <span className="flex-1 text-sm sm:flex-none">{date}</span>
+          <span className="flex-none text-sm">{date}</span>
         </div>
       </div>
       {content}
