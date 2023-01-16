0d28f9171ab8deaf50d76227caa8217c28d1dfa1
Dallas Johnson <dallasajohnson14@gmail.com>
2023-01-16 13:41:11 -0800

    Tweak font colors for readability and emphasis

--- src/components/Sections/Resume/TimelineItem.tsx
+++ src/components/Sections/Resume/TimelineItem.tsx
@@ -5,13 +5,13 @@ import {TimelineItemProps} from '../../../data/dataDef';
 const TimelineItem: FC<{item: TimelineItemProps}> = memo(({item}) => {
   const {title, date, location, content} = item;
   return (
-    <div className="flex flex-col pb-8 text-left last:pb-0">
+    <div className="flex flex-col pb-8 text-left last:pb-0 text-gray-300">
       <div className="flex flex-col pb-4">
-        <h2 className="text-xl font-bold text-center md:text-left">{title}</h2>
+        <h2 className="text-xl font-bold text-center md:text-left text-white">{title}</h2>
         <div className="flex items-center justify-center gap-x-2 md:justify-start">
-          <span className="flex-none text-sm font-medium italic">{location}</span>
+          <span className="flex-none text-sm font-medium italic text-white">{location}</span>
           <span>•</span>
-          <span className="flex-none text-sm">{date}</span>
+          <span className="flex-none text-sm text-gray-300">{date}</span>
         </div>
       </div>
       {content}
