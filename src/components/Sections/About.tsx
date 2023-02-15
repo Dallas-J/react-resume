77f943828cb9990cf588a24e1f50824ec712f73a
Dallas Johnson <dallasajohnson14@gmail.com>
2023-02-14 20:03:10 -0800

    Match photo border to button color

--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -9,7 +9,7 @@ const About = memo(() => {
     <Section className="bg-neutral-100 dark:bg-neutral-800" sectionId={SectionId.About}>
       <div className='grid grid-cols-1 gap-y-4 md:grid-cols-4'>
         <div className="col-span-1 flex justify-center md:justify-start">
-          <div className="h-24 w-24 rounded-xl md:h-32 md:w-32 bg-cover bg-center bg-profile border-2 border-orange-400"/>
+          <div className="h-24 w-24 rounded-xl md:h-32 md:w-32 bg-cover bg-center bg-profile border-2 border-orange-500"/>
         </div>
         <div className='col-span-1 flex flex-col gap-y-6 md:col-span-3'>
           <div className="flex flex-col gap-y-2">
