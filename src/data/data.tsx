c4d1f4d96ff11f87e028abbf85062038d2a7b9b4
Dallas Johnson <dallasajohnson14@gmail.com>
2024-10-10 10:09:00 -0700

    Move resume to public folder to avoid hash in name

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -8,7 +8,6 @@ import {
   SparklesIcon,
 } from '@heroicons/react/24/outline';
 
-import Resume from "%PUBLIC_URL%/Dallas Johnson's Resume.docx";
 import GithubIcon from '../components/Icon/GithubIcon';
 //import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
@@ -87,7 +86,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: Resume,
+      href: process.env.PUBLIC_URL + "/Dallas Johnson's Resume.docx",
       text: 'Resume',
       primary: true,
       link: true,
